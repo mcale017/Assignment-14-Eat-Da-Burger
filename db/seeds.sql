26fa1344eee92e51759eb37b2c96233f9c97cf25
Ryan <mcale017@umn.edu>
2018-06-19 23:40:02 -0500

    db, config, model complete

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheese');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Butter', true);
